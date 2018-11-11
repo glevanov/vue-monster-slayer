d4962f7c045f7e56085fbb0383391e83d0460869
Grigoriy Levanov <glevanov@gmail.com>
2018-11-11 22:21:22 +0300

    Implements attack method

--- app.js
+++ app.js
@@ -14,7 +14,26 @@ new Vue({
       this.health.monster = 100;
     },
     attack: function () {
-      
+      var maxDamage = 15;
+      var minDamage = 1;
+      var damage = Math.floor(Math.random() + maxDamage - minDamage) + minDamage;
+      this.health.monster -= damage;
+
+      if (this.health.monster <= 0) {
+        alert('Victory!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      maxDamage = 17;
+      minDamage = 3;
+      damage = Math.floor(Math.random() + maxDamage - minDamage) + minDamage;
+      this.health.player -= damage;
+
+      if (this.health.player <= 0) {
+        alert('Defeat!');
+        this.gameIsRunning = false;
+      }
     },
     specialAttack: function () {
       
