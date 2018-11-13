5f8a6d498be55909aa183f3df18e15bf5940ff84
Grigoriy Levanov <glevanov@gmail.com>
2018-11-13 21:00:24 +0300

    Fails to add Vue from npm, resorts to local import

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 import "../sass/main.scss";
+import Vue from "./vue.js";
+
+new Vue({
+  el: "#app",
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    isRunning: false
+  },
+  methods: {
+    startGame() {
+      this.isRunning = true;
+    }
+  }
+});
