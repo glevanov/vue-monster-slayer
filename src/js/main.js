39100d676e58236f4a47349c3bd2edea236e963d
Grigoriy Levanov <glevanov@gmail.com>
2018-11-13 22:06:02 +0300

    Implements end game dialog

--- src/js/main.js
+++ src/js/main.js
@@ -7,24 +7,31 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     isRunning: false,
+    isFinished: false,
+    endGameMessage: "",
     log: []
   },
   methods: {
     startGame() {
       this.isRunning = true;
+      this.isFinished = false;
       this.playerHealth = 100;
       this.monsterHealth = 100;
       this.log = [];
     },
     giveUp() {
       this.isRunning = false;
+      this.isFinished = false;
     },
     attack() {
-      let damage = this._calculateDamage(1, 10);
+      let damage = this._calculateDamage(3, 10);
+      if (damage > this.monsterHealth) {
+        damage = this.monsterHealth;
+      }
       this.monsterHealth -= damage;
       this.log.unshift({
         isPlayer: true,
-        text: 'Player slashes Monster for ' + damage
+        text: "Player slashes Monster for " + damage
       });
       if (this._checkWin()) {
         return;
@@ -32,11 +39,14 @@ new Vue({
       this._monsterAttack();
     },
     special() {
-      let damage = this._calculateDamage(8, 15);
+      let damage = this._calculateDamage(10, 20);
+      if (damage > this.monsterHealth) {
+        damage = this.monsterHealth;
+      }
       this.monsterHealth -= damage;
       this.log.unshift({
         isPlayer: true,
-        text: 'Player blasts Monster with fireball for ' + damage
+        text: "Player blasts Monster for " + damage
       });
       if (this._checkWin()) {
         return;
@@ -51,33 +61,30 @@ new Vue({
       }
       this.log.unshift({
         isPlayer: true,
-        text: 'Player heals for 10'
+        text: "Player heals for 10"
       });
       this._monsterAttack();
     },
     _monsterAttack() {
       let damage = this._calculateDamage(3, 12);
+      if (damage > this.playerHealth) {
+        damage = this.playerHealth;
+      }
       this.playerHealth -= damage;
       this._checkWin();
       this.log.unshift({
         isPlayer: false,
-        text: 'Monster claws Player for ' + damage
+        text: "Monster claws Player for " + damage
       });
     },
     _checkWin() {
       if (this.monsterHealth <= 0) {
-        if (confirm('You won! New Game?')) {
-          this.startGame();
-        } else {
-          this.isRunning = false;
-        }
+        this.endGameMessage = "Victory! Start new game?";
+        this.isFinished = true;
         return true;
       } else if (this.playerHealth <= 0) {
-        if (confirm('You lost! New Game?')) {
-          this.startGame();
-        } else {
-          this.isRunning = false;
-        }
+        this.endGameMessage = "Defeat! Start new game?";
+        this.isFinished = true;
         return true;
       }
       return false;
